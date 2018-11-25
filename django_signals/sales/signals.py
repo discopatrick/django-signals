8e9c40a7d9244c1010b6b52a98335b6a0de91189
discopatrick <discopatrick@gmail.com>
2018-11-25 21:00:51 +0000

    Add receivers for Sale pre_save and post_save signals

--- django_signals/sales/signals.py
+++ django_signals/sales/signals.py
@@ -1,10 +1,21 @@
 import logging
 
 from django.core.signals import request_finished
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+
+from sales.models import Sale
 
 logger = logging.getLogger(__name__)
 
 @receiver(request_finished)
 def log_request_finished(sender, **kwargs):
-    logger.info('Request finished.')
+    print('Request finished.')
+
+@receiver(pre_save, sender=Sale)
+def log_sale_pre_save(sender, **kwargs):
+    print('Sale about to save.')
+
+@receiver(post_save, sender=Sale)
+def log_sale_post_save(sender, **kwargs):
+    print('Sale finished saving.')
